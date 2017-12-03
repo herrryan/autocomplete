7a4dc8a0c806323e4f24c9f654f5ed3c4ef364a2
Fei Guo <guofei89@gmail.com>
2017-12-03 20:17:56 +0100

    front end

--- get-dictionary.py
+++ get-dictionary.py
@@ -1,11 +1,14 @@
+
 import json
 
-def load_dataset(file):
-    with open(file) as dataset:
+def convert_dataset_to_productname_dict(inputfile, outputfile):
+    with open(inputfile) as dataset:
         data = json.load(dataset)
     for entry in data:
 	    print entry['name']
-	
-	
+	    with open(outputfile, 'a') as output:
+	        output.write(entry['name'])
+			
+			
 if __name__ == "__main__":
-    load_dataset('products.json')
+    convert_dataset_to_productname_dict('products.json', 'product_name_dict.txt')
